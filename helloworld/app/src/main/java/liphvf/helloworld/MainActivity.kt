42ae1b698c73088a98a4ec61cca6ae0f2fae7e34
Filiphe Vilar Figueiredo <liphvf@gmail.com>
2019-03-05 23:38:36 -0300

    fluxo básico, completo.

--- helloworld/app/src/main/java/liphvf/helloworld/MainActivity.kt
+++ helloworld/app/src/main/java/liphvf/helloworld/MainActivity.kt
@@ -2,6 +2,7 @@ package liphvf.helloworld
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,21 +15,29 @@ class MainActivity : AppCompatActivity() {
 
         btn_calcular.setOnClickListener {
 
-//            Toast.makeText(this, "Test", Toast.LENGTH_SHORT )
             var valorSalario = 0.0;
-
-            if (!txt_valor_salario.text.isEmpty()) {
-                valorSalario = txt_valor_salario.text.toString().toDouble()
+            if (txt_valor_salario.text.isEmpty()) {
+                Toast.makeText(this, "Valor do salário não definido.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
             }
+            valorSalario = txt_valor_salario.text.toString().toDouble()
 
-            var horasTrabalhaadas = txt_horas_trabalhadas.text.toString().toDouble()
+            var horasTrabalhaadas = 0.0;
+            if (txt_horas_trabalhadas.text.isEmpty()) {
+                Toast.makeText(this, "Valor de horas trabalhadas não definido.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            horasTrabalhaadas = txt_horas_trabalhadas.text.toString().toDouble()
 
-            var valorProduto = txt_valor_produto.text.toString().toDouble()
+            var valorProduto = 0.0;
+            if (txt_valor_produto.text.isEmpty()) {
+                Toast.makeText(this, "Valor do produto não definido.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            valorProduto = txt_valor_produto.text.toString().toDouble()
 
             var resultado = valorProduto / (valorSalario/ horasTrabalhaadas)
-
             txt_resultado.text = "Você gastará $resultado horas de trabalho para obter esse produto.";
-
         }
     }
 }

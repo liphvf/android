de47550f38efc278c88b94f4f06b1ceeecf83ac5
Filiphe Vilar Figueiredo <liphvf@gmail.com>
2019-03-05 23:21:39 -0300

    init

--- helloworld/app/src/main/java/liphvf/helloworld/MainActivity.kt
+++ helloworld/app/src/main/java/liphvf/helloworld/MainActivity.kt
@@ -1,12 +1,34 @@
 package liphvf.helloworld
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        btn_calcular.setOnClickListener {
+
+//            Toast.makeText(this, "Test", Toast.LENGTH_SHORT )
+            var valorSalario = 0.0;
+
+            if (!txt_valor_salario.text.isEmpty()) {
+                valorSalario = txt_valor_salario.text.toString().toDouble()
+            }
+
+            var horasTrabalhaadas = txt_horas_trabalhadas.text.toString().toDouble()
+
+            var valorProduto = txt_valor_produto.text.toString().toDouble()
+
+            var resultado = valorProduto / (valorSalario/ horasTrabalhaadas)
+
+            txt_resultado.text = "Você gastará $resultado horas de trabalho para obter esse produto.";
+
+        }
     }
 }
